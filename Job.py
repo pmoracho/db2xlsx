071d07bdf64628032f9dc8703fe867ecbf3ce022
Patricio Moracho <pmoracho@gmail.com>
2020-11-13 21:24:18 -0300

    nada importante

---
+++ Job.py
@@ -0,0 +1,67 @@
+""
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+# Copyright (c) 2014 Patricio Moracho <pmoracho@gmail.com>
+#
+# db2xlsx.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License
+# as published by the Free Software Foundation. A copy of this license should
+# be included in the file GPL-3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+"""
+__author__        = "Patricio Moracho <pmoracho@gmal.com>"
+__appname__        = "autoxls"
+__appdesc__        = "Generación automatizada de archivos Excel"
+__license__        = 'GPL v3'
+__copyright__    = "2019 %s" % (__author__)
+__version__        = "0.9"
+__date__        = "2019/04/08"
+
+"""
+###############################################################################
+# Imports
+###############################################################################
+"""
+"""
+# Copyright (c) 2014 Patricio Moracho <pmoracho@gmail.com>
+#
+# db2xlsx.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License
+# as published by the Free Software Foundation. A copy of this license should
+# be included in the file GPL-3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+"""
+__author__        = "Patricio Moracho <pmoracho@gmal.com>"
+__appname__        = "autoxls"
+__appdesc__        = "Generación automatizada de archivos Excel"
+__license__        = 'GPL v3'
+__copyright__    = "2019 %s" % (__author__)
+__version__        = "0.9"
+__date__        = "2019/04/08"
+
+"""
+###############################################################################
+# Imports
+###############################################################################
+"""
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # db2xlsx
 Database to Xlsx (Reencarnación de autoxls)
+
+## Instalción de desarrollo
+
+1. Clonar repositorio:
+
+    git clone https://github.com/pmoracho/db2xlsx.git
+
+2. Crear entorno virtual:
+
+    python -m venv venv --prompt db2xls
